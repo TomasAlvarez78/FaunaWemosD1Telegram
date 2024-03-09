85f79f33fcc0f781901a67a6077244a68b3dab4e
Tomas Alvarez <alvareztomasignacio@gmail.com>
2024-03-09 15:43:04 -0300

    Variable timer and moistureValue added

--- WemosD1Telegram.ino
+++ WemosD1Telegram.ino
@@ -6,7 +6,7 @@
 #define sensorPin A0
 
 const unsigned long timeBetweenMessageRead = 1000;
-const unsigned long timeBetweenMessageSent = 60000;
+unsigned long timeBetweenMessageSent = 60000;
 const unsigned long timeBetweenSensor = 1000;
 
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
@@ -20,12 +20,18 @@ unsigned long lastTimeSensor;
 const int ledPin = LED_BUILTIN;
 int ledStatus = 0;
 
+#define moistureMin 370
+#define moistureMax 1000
+
+int moistureMiddleValue = 600;
+
 void sendMessage(String message) {
   String chatId = bot.messages[0].chat_id;
-
-  String message2 = message + ".\n";
-
-  bot.sendMessage(chatId, message2, "");
+    if (millis() - lastTimeSent > timeBetweenMessageSent) {
+      Serial.println("Sending message");
+      bot.sendMessage(chatId, message, "");
+      lastTimeSent = millis();
+    }
 }
 
 // If the sensor value is 1000 or more than that then the sensor is not in the soil or sensor is disconnected.
@@ -33,17 +39,25 @@ void sendMessage(String message) {
 // If the sensor value is 370 to 600 then the soil is humid.
 // If the sensor value is less than 370 then the sensor in the water.
 
-void checkMoisture(){
+int checkMoisture(){
   int moistureValue = analogRead(sensorPin);
   Serial.println("Moisture value: " + String(moistureValue));
 
-  if (moistureValue > 500){
-    if (millis() - lastTimeSent > timeBetweenMessageSent) {
-      Serial.println("Sending message");
-      sendMessage("Water your plant...");
-      lastTimeSent = millis();
-    }
+  // Check for possible errors in sensor
+  if (moistureValue < moistureMin){
+    sendMessage("Sensor in water?\nHello I'm under the water please help me");
+    return 0 ;
+  }else if (moistureValue > moistureMax){
+    sendMessage("Sensor not in soil or disconnected");
+    return 0 ;
   }
+
+  // Normal moisture logic
+  if (moistureValue > moistureMiddleValue){
+      sendMessage("Water your plant...");
+  }
+
+  return moistureValue;
 }
 
 void handleNewMessages(int numNewMessages) {
@@ -61,30 +75,59 @@ void handleNewMessages(int numNewMessages) {
       digitalWrite(ledPin, LOW);
       ledStatus = 1;
       bot.sendMessage(chatId, "Led is ON", "");
+      return;
     }
 
     if (text == "/ledoff") {
       ledStatus = 0;
       digitalWrite(ledPin, HIGH);
       bot.sendMessage(chatId, "Led is OFF", "");
+      return;
+    } 
+
+    if (text.substring(0,12) == "/setMoisture") {
+      // Serial.print("String lenght: " + String(text.length()));
+      String tempMoisture = text.substring(12,text.length());
+      tempMoisture.trim();
+      Serial.println(tempMoisture);
+      moistureMiddleValue = tempMoisture.toInt(); 
+      bot.sendMessage(chatId, "Moisture set to " + tempMoisture, "");
+      return;
+    }
+
+    if (text.substring(0,10) == "/setTimer") {
+      // Serial.print("String lenght: " + String(text.length()));
+      String tempTimer = text.substring(10,text.length());
+      tempTimer.trim();
+      Serial.println(tempTimer);
+      timeBetweenMessageSent = tempTimer.toInt();
+      bot.sendMessage(chatId, "Timer set to " + tempTimer, "");
+      return;
     }
 
     if (text == "/status") {
       if (ledStatus) {
-        bot.sendMessage(chatId, "Led is ON", "");
+        bot.sendMessage(chatId, "Led is ON and soil is " + String(checkMoisture()), "");
       } else {
-        bot.sendMessage(chatId, "Led is OFF", "");
+        bot.sendMessage(chatId, "Led is OFF and soil is " + String(checkMoisture()), "");
       }
+      return;
     }
+
 
     if (text == "/start") {
       String welcome = "Welcome to Universal Arduino Telegram Bot library, " + fromName + ".\n";
       welcome += "This is Flash Led Bot example.\n\n";
       welcome += "/ledon : to switch the Led ON\n";
       welcome += "/ledoff : to switch the Led OFF\n";
-      welcome += "/status : Returns current status of LED\n";
+      welcome += "/setMoisture {value} : Set the value for moisture alarm\n";
+      welcome += "/setTimer {value} : Set the timer for moisture alarm ( ms )\n";
+      welcome += "/status : Returns current status of LED and soil \n";
       bot.sendMessage(chatId, welcome, "Markdown");
+      return;
     }
+
+
   }
 }
 
